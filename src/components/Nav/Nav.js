963097f78883e3889dd3f718955106afa5055042
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-11-16 21:41:24 -0700

    Adds in shell of drawer

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,6 +7,14 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
 
 const styles = {
   root: {
@@ -22,17 +30,64 @@ const styles = {
 };
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleDrawer() {
+   this.setState({
+     open: !this.state.open
+   });
+  };
+
   render() {
     const { classes } = this.props;
 
+    const sideList = (
+      <div className={classes.list}>
+        <List>
+            <ListItem button>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary="About" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary="Geologic Timeline" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary="Field Trip Guide" />
+            </ListItem>
+        </List>
+      </div>
+    );
+
     return (
       <div className={classes.root}>
+      <SwipeableDrawer
+          open={this.state.open}
+          onClose={() => this.toggleDrawer()}
+          onOpen={() => this.toggleDrawer()}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={() => this.toggleDrawer()}
+            onKeyDown={() => this.toggleDrawer()}
+          >
+            {sideList}
+          </div>
+        </SwipeableDrawer>
         <AppBar position="fixed" color="secondary">
           <Toolbar>
             <IconButton
               className={classes.menuButton}
               color="inherit"
               aria-label="Menu"
+              onClick={() => this.toggleDrawer()}
             >
               <MenuIcon />
             </IconButton>
